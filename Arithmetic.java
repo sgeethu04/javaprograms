f9b53eefbf428242f04ac9989a70e805396ef9a6
sgeethu04 <110230576+sgeethu04@users.noreply.github.com>
2023-02-22 20:36:54 +0530

    arithmetic operations

---
+++ Arithmetic.java
@@ -0,0 +1,21 @@
+class Arithmetic{
+	 int n=8;
+public static void main(String args[]){
+	
+	//(10*20+(98*445)+
+	//Arithmetic obj = new Arithmetic();
+System.out.println(sum(10,20));
+
+}
+
+
+
+public static int sum(int a, int b){
+int sum= a+b;
+
+System.out.println("Sum="+n);
+return sum;
+
+}
+
+}
